12711521004c8f5c1e36f6772ad56a691512e4b5
Cielo Raymundo <craymundo.student@hsfi.us>
2017-03-08 22:08:50 +0000

    idk

--- script.js
+++ script.js
@@ -1,18 +1,39 @@
 TinyTurtle.apply(window, [undefined, 400, 400]);
 
-function square(){
-forward(50);
+function rectangle(size){
+forward(size);
 right(90);
-forward(50);
+forward(size);
 right(90);
-forward(50);
+forward(size);
 right(90);
-forward(50);
+forward(size);
 right(90);
-forward(50);
+forward(size);
 }
+
 
 // Type your function call below
 
+
+function triangle(size){
+    right(45);
+    forward(size);
+    right(90);
+    forward(size);
+    right(135);
+    forward(size);
+}
+
+
+function shape(type, size) {
+    if (type==='triangle'){
+   triangle(size);
+}
+}
+
+
+shape('triangle',70);
+shape('rectangle',80);
 
 stamp();
